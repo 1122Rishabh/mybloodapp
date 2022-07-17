bf11f29145b8e29cdb659df2bd32fa6939a227a3
1122Rishabh <rishabhbansal376@gmail.com>
2022-07-17 07:45:50 -0700

    dd

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://rishi1122:rishi1122@cluster0.gvop3b1.mongodb.net/test",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
    // useCreateIndex:true
 }).then(()=>{
     console.log(`Connection Succesfully`);
