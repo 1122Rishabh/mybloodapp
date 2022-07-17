5dd864e4dddebf448345fa9dd76f155926d07781
1122Rishabh <rishabhbansal376@gmail.com>
2022-07-16 23:03:19 -0700

    ii

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ require("./src/db/conn");
 const Register=require("./src/models/registers")
 const port= process.env.PORT || 5000
 const static_path=path.join(__dirname,"../public")
-const template_path=path.join(__dirname,"../templates/views");
-const partials_path=path.join(__dirname,"../templates/partials");
+const template_path=path.join(__dirname,"./templates/views");
+const partials_path=path.join(__dirname,"./templates/partials");
 
 
 
